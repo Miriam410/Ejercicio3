9d8e82c65ef18231bc96e035193f338f326dc299
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-03-20 17:43:39 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Precione las teclas.");
+            char tecla = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Las teclas presionadas son: "+ tecla);
+            Console.WriteLine("Para finalizar, presione la tecla: X ");
+            Console.ReadLine();
+        }
+    }
+}
